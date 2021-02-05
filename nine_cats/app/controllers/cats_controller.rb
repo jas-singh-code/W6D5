b6537e0494ec2322619ff998bf25379422c85c28
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-02-05 17:06:14 -0500

    finished controller

--- nine_cats/app/controllers/cats_controller.rb
+++ nine_cats/app/controllers/cats_controller.rb
@@ -1,47 +1,49 @@
-class Cats_Controller < ApplicationController
+class CatsController < ApplicationController
+
     def index
-        cats = Cat.all 
-        render json: cats
+        @cats = Cat.all 
+        render :index
     end
 
     def show
-        cat = Cat.find_by(id: params[:id])
-        render json: cat 
-    end    
+        @cat = Cat.find_by(id: params[:id])
+        render :show
+    end
+    
+    def new
+        @cat = Cat.new(cat_params)
+        render :new
+    end
 
     def create
-        cat = Cat.new(cat_params)
-        if cat.save! 
-            redirect_to cat_url(cat.id)  
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat.id)  
         else
-            render json: cat.errors.full_messages, status: 307
+            render json: @cat.errors.full_messages, status: 422
         end 
     end
 
     # def edit 
-    #     cat = find_by(id: params[:id])
-
+    #     @cat = find_by(id: params[:id])
     # end
 
     def update
-        cat = Cat.find_by(id: params[:id])
-        cat.update(cat_params)
-        if cat.save! 
-            redirect_to cat_url(cat.id)
+        @cat = Cat.find(params[:id])
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat.id)
         else
-            render json: cat.errors.full_messages, status: 307 
+            render json: @cat.errors.full_messages, status: 422
         end
     end
 
     def destroy 
-        cat = Cat.find(params[:id]
-        cat.destroy
-        render json: cat 
+        @cat = Cat.find(params[:id])
+        render json: @cat.destroy
     end
 
     private 
-    
     def cat_params
-        params.require(:cat).permit(:name, :birth_date, :color, :description, :sex)
+        params.require(:@cat).permit(:name, :birth_date, :color, :description, :sex)
     end
 end
--- nine_cats/db/seeds.rb
+++ nine_cats/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cats = Cat.create(
+    [
+        {name: "Garfield", color: "black", sex: "M", birth_date: "1922/04/05", description: "its Garfield"},
+        {name: "Kitty", color: "white", sex: "F", birth_date: "2000/04/05", description: "its Kitty"},
+        # {name: "Cat-Rich", color: "grey", sex: "M", birth_date: "2010/04/05", description: "its Cat-Rich"},
+        {name: "Son-cat", color: "black", sex: "F", birth_date: "2020/04/05", description: "its Son-cat"},
+    ]
+)
